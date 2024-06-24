57c28f7e5a5a50b9a2f632a3430513b7fbbb1604
DemiGarf <117126917+DemiGarf@users.noreply.github.com>
2024-06-24 11:51:12 -0300

    Clase 9

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default fuction ({Pantalla})() ; {
+    return (
+        <Router>
+        <Routes>
+        <Route path="/" element={<Pantalla />} />
+        </Routes>
+        </Router>
+    )
+}
---
+++ src/pantallaej.jsx
@@ -0,0 +1,13 @@
+export default fuction ({Pantalla})(); {
+    return (
+        <div className="home-page">
+        <div className="container">
+        <h1 className=" title">Hola Demi</h1>
+        <div className="grid">
+        </div>
+     </div>
+</div>
+
+
+    );
+}
