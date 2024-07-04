830c9a08be439be2c6a095343cb8dfa9a07f1b99
DemiGarf <117126917+DemiGarf@users.noreply.github.com>
2024-07-04 12:02:40 -0300

    Uriel

--- src/components/EjercicioLista.tsx
+++ src/components/EjercicioLista.tsx
@@ -1,63 +1,35 @@
-//
-import React from 'react';
-//import ejerciciosRoutes from './Routes/ejercicios.js'
-
-/*
-const [ejercicios, setEjercicios] = useState([]);
-
-useEffect(() => {
-    const obtenerNombresEjercicios = async () => {
-        try {
-            const response = await fetch('/api/ejercicios/nombres');
-            if (!response.ok) {
-                throw new Error('Error al obtener los nombres de los ejercicios');
-            }
-            const data = await response.json();
-            console.log(data);
-            setEjercicios(data);
-        } catch (error) {
-            console.error('Error al obtener los nombres de los ejercicios:', error);
-        }
-    };
-
-    obtenerNombresEjercicios();
-}, []);
-*/
-
-//TODO: REEMPLAZAR POR API
-const ejercicios = [
-    {
-        Id: "1",
-        Nombre: "Bench press"
-    },
-    {
-        Id: "2",
-        Nombre: "Curl biceps"    
-    },
-    {
-        Id: "3",
-        Nombre: "Curl triceps"
-    },
-    {
-        Id: "4",
-        Nombre: "Polea triceps"
-    }
-];
+import React, { useEffect, useState } from 'react';
 
 const EjercicioLista: React.FC = () => {
+    const [ejercicios, setEjercicios] = useState<{ Id: string; Nombre: string; }[]>([]);
+
+    useEffect(() => {
+        const fetchEjercicios = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/ejercicios/nombres');
+                const data = await response.json();
+                setEjercicios(data);
+            } catch (error) {
+                console.error('Error fetching exercises:', error);
+            }
+        };
+
+        fetchEjercicios();
+    }, []);
+
     return (
         <div id="ejerciciolista">
             <h2 className="main-title">Exercises</h2>
             <div className="ejercicios-listado-container row">
                 {ejercicios.map(ejercicio => (
                     <div key={ejercicio.Id} className="col-12 col-md-6">
-                        <a href={`/ejercicio?id=${ejercicio.Id}`} className="">
+                        <a href={`/ejercicio?id=${ejercicio.Nombre}`} className="">
                             <div className="ejercicio-listado-item">
                                 {ejercicio.Nombre}
                             </div>
                         </a>
                     </div>
-                ))}   
+                ))}
             </div>
         </div>
     );
