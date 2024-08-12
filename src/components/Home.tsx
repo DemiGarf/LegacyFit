6f3e92d83cae1c3a93874f0d5315440f2de03ba5
DemiGarf <117126917+DemiGarf@users.noreply.github.com>
2024-08-12 11:36:39 -0300

    ayuda marto 12/8

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,17 +4,17 @@ import "../App.css"
 
 const Home: React.FC = () => {
     return (
-        <div id="home">
-            <h2 className="main-title">HOME</h2>
+        <div className="home">
+            <div>
+                <h2 className="main-title">HOME</h2>
+            </div>
             <div>
                 <ul>
                     <li><a href="/lista" className="lista-button">LISTA</a></li>
-
-                    <li><a href="/ejercicio" className="ejercicio-button">EJERCICIO</a></li>
                 </ul>
             </div>
         </div>
-    );
+    );  
 };
 
 export default Home;
