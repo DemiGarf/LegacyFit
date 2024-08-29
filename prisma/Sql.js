d7a1bfd6aa79a1835f5c8b9d142d9ac83b9a504e
DemiGarf <117126917+DemiGarf@users.noreply.github.com>
2024-08-29 10:14:10 -0300

    lycas

--- prisma/Sql.js
+++
@@ -1,2 +0,0 @@
-// cada vez que haga algo en prisma tengo que poner: npx prisma migrate dev --name init y despues npx prisma generate
-//DATABASE_URL= "postgres://postgres.jbnqlmrwypswfrtaflrm:Legacy2%3A%29%23%2Fla@aws-0-sa-east-1.pooler.supabase.com:5432/postgres?pgbouncer=true"
