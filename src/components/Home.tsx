86c069e40d0ca4931245aa2bb80f612ac66d3164
DemiGarf <117126917+DemiGarf@users.noreply.github.com>
2024-09-24 08:55:58 -0300

    Incio de sesion falta css

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,35 @@
-// Pantalla1.tsx
 import React from 'react';
 import "../App.css"
+
 
 const Home: React.FC = () => {
     return (
         <div className="home">
-            <div>
-                <h2 className="main-title">HOME</h2>
-            </div>
-            <div>
-                <ul>
-                    <li><a href="/lista" className="lista-button">LISTA</a></li>
-                </ul>
+            <h2 className="main-title">Inicio de sesión</h2> {/* Título fuera del contenedor del formulario */}
+            <div className="login-container">
+                <form className="login-form">
+                    <div className="form-group">
+                        <label htmlFor="nombre">Nombre</label>
+                        <input type="text" id="nombre" placeholder="Ingresa tu nombre" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="apellido">Apellido</label>
+                        <input type="text" id="apellido" placeholder="Ingresa tu apellido" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" placeholder="Ingresa tu email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Contraseña</label>
+                        <input type="password" id="password" placeholder="Ingresa tu contraseña" />
+                    </div>
+                    <button type="submit" className="submit-button">Iniciar sesión</button>
+                </form>
             </div>
         </div>
-    );  
+    );
 };
+
 
 export default Home;
