3b4020aaa2124e0b3bd7e087cd315f83c8b4b33b
DemiGarf <117126917+DemiGarf@users.noreply.github.com>
2024-10-14 11:56:58 -0300

    inicio de sesion con presi

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,38 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../App.css"
 
-
 const Home: React.FC = () => {
+    const [Nombre, setNombre] = useState('');
+    const [Apellido, setApellido] = useState('');
+    const [Mail, setMail] = useState('');
+    const [Contrasena, setContrasena] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const userData = {
+            Nombre,
+            Apellido,
+            Mail,
+            Contrasena,
+        };
+
+        try {
+            const response = await fetch('https://legacy-fit-pp4p.vercel.app/user/Create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            });
+
+            if (response.ok) {
+                console.log('Usuario registrado con éxito');
+                // Redireccionar o mostrar mensaje de éxito
+                // ejercicioLista
+                window.location.href = "/lista"
+            } else {
+                console.error('Error al registrar el usuario');
+            }
+        } catch (error) {
+            console.error('Error en la petición:', error);
+        }
+    };
+
     return (
         <div className="home">
-            <h2 className="main-title">Registrarse</h2> {/* Título fuera del contenedor del formulario */}
+            <h2 className="main-title">Registrarse</h2>
             <div className="login-container">
-                <form action='/lista' method='GET' className="login-form">
+                <form onSubmit={handleSubmit} className="login-form">
                     <div className="form-group">
                         <label htmlFor="nombre">Nombre</label>
-                        <input type="text" id="nombre" placeholder="Ingresa tu nombre" />
+                        <input
+                            type="text"
+                            id="nombre"
+                            placeholder="Ingresa tu Nombre"
+                            value={Nombre}
+                            onChange={(e) => setNombre(e.target.value)}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="apellido">Apellido</label>
-                        <input type="text" id="apellido" placeholder="Ingresa tu apellido" />
+                        <input
+                            type="text"
+                            id="apellido"
+                            placeholder="Ingresa tu Apellido"
+                            value={Apellido}
+                            onChange={(e) => setApellido(e.target.value)}
+                        />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" placeholder="Ingresa tu email" />
+                        <label htmlFor="mail">Mail</label>
+                        <input
+                            type="email"
+                            id="mail"
+                            placeholder="Ingresa tu Mail"
+                            value={Mail}
+                            onChange={(e) => setMail(e.target.value)}
+                        />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="password">Contraseña</label>
-                        <input type="password" id="password" placeholder="Ingresa tu contraseña" />
+                        <label htmlFor="contrasena">Contrasena</label>
+                        <input
+                            type="password"
+                            id="contrasena"
+                            placeholder="Ingresa tu Contrasena"
+                            value={Contrasena}
+                            onChange={(e) => setContrasena(e.target.value)}
+                        />
                     </div>
                     <button type="submit" className="submit-button">Registrarse</button>
                 </form>
             </div>
             <div>
-                <p className="login-desc">Ya tenes cuenta? &nbsp; &nbsp; <a href="/iniciar-session">Inicia Session</a></p>
+                <p className="login-desc">¿Ya tienes cuenta? &nbsp; &nbsp; <a href="/iniciar-session">Inicia Sesión</a></p>
             </div>
         </div>
     );
 };
-
 
 export default Home;
--- src/components/InicioSession.tsx
+++ src/components/InicioSession.tsx
@@ -1,22 +1,57 @@
-
-
+import { useState } from 'react';
 import React from 'react';
 import "../App.css"
 
 
 const InicioSession: React.FC = () => {
+    const [Mail, setMail] = useState('');
+    const [Contrasena, setContrasena] = useState('');
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const userData = {
+            Mail,
+            Contrasena,
+        };
+
+        try {
+            const response = await fetch('https://legacy-fit-pp4p.vercel.app/user/Create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            });
+
+            if (response.ok) {
+                console.log('Usuario registrado con éxito');
+                // Redireccionar o mostrar mensaje de éxito
+                // ejercicioLista
+                window.location.href = "/lista"
+            } else {
+                console.error('Error al registrar el usuario');
+            }
+        } catch (error) {
+            console.error('Error en la petición:', error);
+        }
+    };
     return (
         <div className="home">
             <h2 className="main-title">Inicio Sesion</h2> {/* Título fuera del contenedor del formulario */}
             <div className="login-container">
-                <form action='/lista' method='GET' className="login-form">
+                <form onSubmit={handleSubmit} className="login-form">
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="text" id="email" placeholder='email o telefono'/>
+                        <input 
+                        type="email"
+                        value={Mail}
+                            onChange={(e) => setMail(e.target.value)}
+                         id="email" placeholder='email o telefono'/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Contraseña</label>
-                        <input type="password" id="password" placeholder="Ingresa tu contraseña" />
+                        <input value={Contrasena} onChange={(e) => setContrasena(e.target.value)}
+                         type="password" id="password" placeholder="Ingresa tu contraseña" />
                     </div>
                     <button type="submit" className="submit-button">Iniciar Sesion</button>
                 </form>
